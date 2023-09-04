8d109de6149caeb1810f5554e928dc2d5522cf4c
Chris King <me@chrisking.io>
2023-09-04 00:30:58 -0500

    Add pyenv on startup

---
+++ run_onchange_install-pyenv.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+PYENV_SETUP=$(cat << "EOF"
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+EOF
+)
+
+##
+# Install pyenv
+##
+
+if [ ! -d ${HOME}/.pyenv ]; then
+    curl https://pyenv.run | bash
+
+    # make sure that .bashrc is up-to-date
+    echo $PYENV_SETUP >> ~/.bashrc
+fi
+
+# Just in case we're in a shell that doesn't respect .bashrc
+eval "$PYENV_SETUP"
+
+# Install python versions
+pyenv install 3.11.5
+pyenv install 3.10.13
